0a3372166f8cf90643b42c6fa9aa87f3ca83a0a4
Dongwon Kang <dongwon.dwk@gmail.com>
2024-11-06 15:10:39 -0800

    modified header and navigation

--- header.php
+++ header.php
@@ -28,7 +28,10 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
+			// custom logo from backend
 			the_custom_logo();
+			// second logo or title manually
+			echo "<p class='sub-logo'>Teaylormade</p>";
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -46,7 +49,18 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'an-store' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<!-- open button -->
+				<svg class="icon-open" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<path d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero" />
+				</svg>
+				<!-- close button -->
+				<svg class="icon-close" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path d="M18.3 5.71a1 1 0 0 0-1.42 0L12 10.59 7.12 5.71a1 1 0 1 0-1.41 1.41L10.59 12l-4.88 4.88a1 1 0 0 0 1.41 1.41L12 13.41l4.88 4.88a1 1 0 0 0 1.42-1.41L13.41 12l4.88-4.88a1 1 0 0 0 0-1.41z" />
+				</svg>
+			</button>
+
+			<!-- menu content -->
 			<?php
 			wp_nav_menu(
 				array(
@@ -56,4 +70,48 @@
 			);
 			?>
 		</nav><!-- #site-navigation -->
+
+		<!-- Cart button -->
+
+		<?php 
+		
+		$cart_url = wc_get_cart_url();
+		$cart_count = WC()->cart->get_cart_contents_count();
+		$cart_icon = '<svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 width="800px" height="800px" viewBox="0 0 902.86 902.86"
+	 xml:space="preserve">
+<g>
+	<g>
+		<path d="M671.504,577.829l110.485-432.609H902.86v-68H729.174L703.128,179.2L0,178.697l74.753,399.129h596.751V577.829z
+			 M685.766,247.188l-67.077,262.64H131.199L81.928,246.756L685.766,247.188z"/>
+		<path d="M578.418,825.641c59.961,0,108.743-48.783,108.743-108.744s-48.782-108.742-108.743-108.742H168.717
+			c-59.961,0-108.744,48.781-108.744,108.742s48.782,108.744,108.744,108.744c59.962,0,108.743-48.783,108.743-108.744
+			c0-14.4-2.821-28.152-7.927-40.742h208.069c-5.107,12.59-7.928,26.342-7.928,40.742
+			C469.675,776.858,518.457,825.641,578.418,825.641z M209.46,716.897c0,22.467-18.277,40.744-40.743,40.744
+			c-22.466,0-40.744-18.277-40.744-40.744c0-22.465,18.277-40.742,40.744-40.742C191.183,676.155,209.46,694.432,209.46,716.897z
+			 M619.162,716.897c0,22.467-18.277,40.744-40.743,40.744s-40.743-18.277-40.743-40.744c0-22.465,18.277-40.742,40.743-40.742
+			S619.162,694.432,619.162,716.897z"/>
+	</g>
+</g>
+</svg>';
+
+		// =====  Cart button START ===== //
+		$cart_button = '<a href="' . esc_url($cart_url) . '" class="cart-button">';
+		$cart_button .= $cart_icon;
+		
+		
+		// add alert dot if any items exists in the cart
+		if ($cart_count > 0) {
+			$cart_button .= '<span class="cart-badge"></span>';
+		}
+
+		$cart_button .= '</a>';
+		// =====  Cart button END ===== //
+		?>
+
+
+		<div class="header-cart-button">
+			<?php echo $cart_button; ?>
+		</div>
+
 	</header><!-- #masthead -->
